1729407b49f9c98b5cea55d7bf63cbae0c2fbe50
root <root@jenkins2.us-central1-a.c.nth-skyline-271419.internal>
2020-04-24 18:40:32 +0000

    lesson 3 commit

---
+++ lesson3/exe1.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+function file_count()
+{
+COUNT=$(ls -l | wc -l)
+ANS=$((COUNT - 1))
+echo "This folder have $ANS file/files"
+}
+file_count
+<<COMMENT
+Write a shell script that consists of a function that display the number of files in the present
+working directory. Name this function "file_count" and call it in your script. If you use a variable
+in your function, remember to make it a local variable.
+Hint: The wcutility is used to count the number of lines, words, and bytes.
+COMMENT
